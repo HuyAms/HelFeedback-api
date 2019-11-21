4bca98ea72f9911cb05feb28b19624cb1b064dca
huy <dinhhuyams@gmail.com>
2019-11-22 00:11:23 +0200

    fix: rename showOnMobile to isForChildren in survey model

--- src/resources/survey/survey.model.ts
+++ src/resources/survey/survey.model.ts
@@ -27,7 +27,7 @@ const surveySchema = new mongoose.Schema(
 					{
 						value: String,
 						imageUrl: String,
-						showOnMobile: Boolean,
+						isForChildren: Boolean,
 					},
 				],
 			},
